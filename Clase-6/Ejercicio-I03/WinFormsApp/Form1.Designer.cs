576d92d334cd1a2f76ee02fd1bcb1e8a47dfd356
Lau Carelli <carelli.lc@gmail.com>
2021-09-07 22:24:06 -0300

    contadorPalabras in progress

---
+++ Clase-6/Ejercicio-I03/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace WinFormsApp
+{
+    partial class FrmContadorPalabras
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.richTxBoxPalabras = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(408, 379);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // richTxBoxPalabras
+            // 
+            this.richTxBoxPalabras.Location = new System.Drawing.Point(0, 0);
+            this.richTxBoxPalabras.Name = "richTxBoxPalabras";
+            this.richTxBoxPalabras.Size = new System.Drawing.Size(508, 373);
+            this.richTxBoxPalabras.TabIndex = 2;
+            this.richTxBoxPalabras.Text = "";
+            // 
+            // frmContadorPalabras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 414);
+            this.Controls.Add(this.richTxBoxPalabras);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frmContadorPalabras";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.RichTextBox richTxBoxPalabras;
+    }
+}
+
---
+++ Clase-6/Ejercicio-I03/WinFormsApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class FrmContadorPalabras : Form
+    { 
+
+        public FrmContadorPalabras()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private Dictionary<string, int> contadorPalabras() 
+        {
+            string texto = richTxBoxPalabras.Text;
+            Dictionary<String, int> dicPalabras = new Dictionary<string, int>();
+            string[] palabras = texto.Split(' ');
+            int contador = 1;
+
+            foreach (string palabra in palabras)
+            {
+                if (dicPalabras.ContainsKey(palabra)) 
+                {
+                    dicPalabras[palabra]++;
+                } else {
+                    dicPalabras.Add(palabra, 1);
+                }
+            }
+
+            return dicPalabras;
+
+        }
+
+        private List<KeyValuePair<string,int>> odernar(Dictionary<string, int> dic) 
+        {
+            List<KeyValuePair<string, int>> list = dic.ToList();
+            list.Sort(CompararElemento);
+
+            return list;
+        }
+
+        private int CompararElemento(KeyValuePair<string, int> primer, KeyValuePair<string,int> segundo) 
+        {
+            return primer.Value - segundo.Value;
+        }
+
+        private void MostrarPodio(List<KeyValuePair<string, int>> podio) 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (podio.Count == 0)
+            {
+                sb.Append("No se ingresaron palabras");
+            }
+            else
+            { 
+                
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show($"La cantidad de palabras son -> {count}");
+        }
+    }
+}
