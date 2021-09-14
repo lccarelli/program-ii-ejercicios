c209bd8c0e7644e9c9c75bae3c56d81bdbd0b99d
Lau Carelli <carelli.lc@gmail.com>
2021-09-13 22:20:46 -0300

    Clase-7

---
+++ Clase-7/Ejercicio-I01/Biblioteca/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Cliente
+    {
+        private string nombre;
+        private int numero;
+
+        public Cliente(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public Cliente(string nombre, int numero)
+        {
+            Nombre = nombre;
+            this.numero = numero;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+
+            set
+            {
+                this.nombre = value;
+            }
+
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return this.numero;
+            }
+        }
+
+        public static bool operator ==(Cliente c1, Cliente c2)
+        {
+            return c1.nombre == c2.nombre && c1.numero == c2.numero ? true : false;
+        }
+
+        public static bool operator !=(Cliente c1, Cliente c2) 
+        {
+            return !(c1 == c2);
+        }
+    }
+}
---
+++ Clase-7/Ejercicio-I01/Biblioteca/Negocio.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Negocio
+    {
+        PuestoAtencion caja;
+        Queue<Cliente> clientes;
+        string nombre;
+
+        private Negocio() 
+        {
+            this.clientes = new Queue<Cliente>();
+            this.caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+        }
+
+        public Negocio(string nombre)
+            :this()
+        {
+            this.nombre = nombre;
+        }
+
+        public static bool operator ==(Negocio n, Cliente c) 
+        {
+            foreach (Cliente cliente in n.clientes)
+            {
+                if (c == cliente) 
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Negocio n, Cliente c) 
+        {
+            return !(n == c);
+        }
+
+        public static bool operator +(Negocio n, Cliente c) 
+        {
+            if (n != c) 
+            {
+                n.clientes.Enqueue(c);
+                return true;
+            }
+            return false;
+        }
+    }
+}
